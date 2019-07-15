188c0808dc215844e11e6f598b7732eeca7fb83c
lakizoli <lakizoli@yahoo.com>
2019-07-15 06:04:02 +0200

    Add gencw command line tool for generating crosswords on pc

---
+++ gencw/gencw/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  gencw
+//
+//  Created by Laki Zoltán on 2019. 07. 15..
+//  Copyright © 2019. Laki Zoltán. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
