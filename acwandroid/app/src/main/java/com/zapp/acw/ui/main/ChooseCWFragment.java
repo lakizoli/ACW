a7ddf687069f34e2dbde8b9dacbee1bf8828c5f4
Zoltán Laki <lakizoli@yahoo.com>
2019-09-06 07:26:31 +0200

    Android: implementing UI features

--- acwandroid/app/src/main/java/com/zapp/acw/ui/main/ChooseCWFragment.java
+++ acwandroid/app/src/main/java/com/zapp/acw/ui/main/ChooseCWFragment.java
@@ -1,24 +1,23 @@
 package com.zapp.acw.ui.main;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.ViewModelProviders;
-
-import android.app.Activity;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+
 import com.zapp.acw.R;
 
-public class ChooseCWFragment extends Fragment {
+public class ChooseCWFragment extends Fragment implements Toolbar.OnMenuItemClickListener {
 
 	private ChooseCWViewModel mViewModel;
 
@@ -39,8 +38,27 @@ public class ChooseCWFragment extends Fragment {
 		mViewModel = ViewModelProviders.of (this).get (ChooseCWViewModel.class);
 
 		FragmentActivity activity = getActivity ();
-		Toolbar toolbar = (Toolbar) activity.findViewById(R.id.toolbar);
-		toolbar.inflateMenu(R.menu.choosecw_menu);
+		Toolbar toolbar = (Toolbar) activity.findViewById (R.id.toolbar);
+		toolbar.inflateMenu (R.menu.choosecw_menu);
+		toolbar.setOnMenuItemClickListener (this);
+
+		OnBackPressedCallback callback = new OnBackPressedCallback (true /* enabled by default */) {
+			@Override
+			public void handleOnBackPressed () {
+			}
+		};
+		requireActivity ().getOnBackPressedDispatcher ().addCallback (this, callback);
 	}
 
+	@Override
+	public boolean onMenuItemClick (MenuItem menuItem) {
+		switch (menuItem.getItemId ()) {
+			case R.id.action_plus:
+				Navigation.findNavController (getView ()).navigate (R.id.ShowDownload);
+				return true;
+			default:
+				break;
+		}
+		return false;
+	}
 }
