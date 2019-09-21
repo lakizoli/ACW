67ebf78648ac4f46b5ca7464a2e5525545d14808
zoli <lakizoli@yahoo.com>
2019-09-21 12:45:05 +0200

    Android: crossword view

--- acwandroid/app/src/main/java/com/zapp/acw/ui/main/CrosswordFragment.java
+++ acwandroid/app/src/main/java/com/zapp/acw/ui/main/CrosswordFragment.java
@@ -1,20 +1,28 @@
 package com.zapp.acw.ui.main;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import com.zapp.acw.R;
+import com.zapp.acw.bll.SavedCrossword;
 
-public class CrosswordFragment extends Fragment {
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+
+public class CrosswordFragment extends Fragment implements Toolbar.OnMenuItemClickListener {
 
 	private CrosswordViewModel mViewModel;
 
@@ -32,7 +40,72 @@ public class CrosswordFragment extends Fragment {
 	public void onActivityCreated (@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated (savedInstanceState);
 		mViewModel = ViewModelProviders.of (this).get (CrosswordViewModel.class);
-		// TODO: Use the ViewModel
+
+		final FragmentActivity activity = getActivity ();
+
+		//Init toolbar
+		Toolbar toolbar = activity.findViewById (R.id.cwview_toolbar);
+		toolbar.inflateMenu (R.menu.cwview_menu);
+		toolbar.setOnMenuItemClickListener (this);
+		toolbar.setNavigationIcon (R.drawable.ic_arrow_back_black_24dp);
+		toolbar.setNavigationOnClickListener (new View.OnClickListener () {
+			@Override
+			public void onClick (View v) {
+				Navigation.findNavController (getView ()).navigateUp ();
+			}
+		});
+
+		OnBackPressedCallback callback = new OnBackPressedCallback (true /* enabled by default */) {
+			@Override
+			public void handleOnBackPressed () {
+				Navigation.findNavController (getView ()).navigateUp ();
+			}
+		};
+		requireActivity ().getOnBackPressedDispatcher ().addCallback (this, callback);
+
+		//Init view model
+		mViewModel.init (getArguments ());
+
+		//Build crossword table
+		buildCrosswordTable (activity);
 	}
 
+	@Override
+	public boolean onMenuItemClick (MenuItem item) {
+		switch (item.getItemId ()) {
+			case R.id.cwview_reset:
+				return true;
+			case R.id.cwview_hint:
+				return true;
+			default:
+				break;
+		}
+		return false;
+	}
+
+	private void buildCrosswordTable (FragmentActivity activity) {
+		TableLayout table = activity.findViewById (R.id.cw_table);
+
+		final float scale = getContext().getResources().getDisplayMetrics().density;
+		int pixels = Math.round (50.0f * scale + 0.5f);
+
+		SavedCrossword savedCrossword = mViewModel.getSavedCrossword ();
+		for (int row = 0; row < savedCrossword.height;++row) {
+			TableRow tableRow = new TableRow (activity);
+			table.addView (tableRow);
+
+			tableRow.setLayoutParams (new LinearLayout.LayoutParams (ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+			for (int col = 0; col < savedCrossword.width;++col) {
+				View cell = new View (activity);
+				tableRow.addView (cell);
+
+				cell.requestLayout ();
+
+				cell.getLayoutParams ().width = pixels;
+				cell.getLayoutParams ().height = pixels;
+				cell.setBackgroundColor ((col % 2 == 0 && row % 2 == 0) || (col % 2 == 1 && row % 2 == 1) ? 0xFFAA0000 : 0xFF00AA00);
+			}
+		}
+	}
 }
--- acwandroid/app/src/main/java/com/zapp/acw/ui/main/CrosswordViewModel.java
+++ acwandroid/app/src/main/java/com/zapp/acw/ui/main/CrosswordViewModel.java
@@ -1,7 +1,32 @@
 package com.zapp.acw.ui.main;
 
+import android.os.Bundle;
+
+import com.zapp.acw.bll.Package;
+import com.zapp.acw.bll.SavedCrossword;
+
+import java.util.ArrayList;
+
 import androidx.lifecycle.ViewModel;
 
 public class CrosswordViewModel extends ViewModel {
-	// TODO: Implement the ViewModel
+	private boolean isMultiLevelGame;
+	private Package currentPackage;
+	private int currentCrosswordIndex;
+	private SavedCrossword savedCrossword;
+	private ArrayList<SavedCrossword> allSavedCrossword;
+
+	public boolean getIsMultiLevelGame () { return isMultiLevelGame; }
+	public Package getCurrentPackage () { return currentPackage; }
+	public int getCurrentCrosswordIndex () { return currentCrosswordIndex; }
+	public SavedCrossword getSavedCrossword () { return savedCrossword; }
+	public ArrayList<SavedCrossword> getAllSavedCrossword () { return allSavedCrossword; }
+
+	public void init (Bundle args) {
+		currentPackage = args.getParcelable ("package");
+		savedCrossword = args.getParcelable ("savedCrossword");
+		currentCrosswordIndex = args.getInt ("currentCrosswordIndex");
+		allSavedCrossword = args.getParcelableArrayList ("allSavedCrossword");
+		isMultiLevelGame = args.getBoolean ("isMultiLevelGame");
+	}
 }
