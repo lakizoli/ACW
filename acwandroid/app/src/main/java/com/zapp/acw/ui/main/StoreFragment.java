fceafd6964d23b06079b50e2da8b909732e55d5f
zoli <lakizoli@yahoo.com>
2019-10-27 22:37:45 +0100

    Android: establish store view appearance

--- acwandroid/app/src/main/java/com/zapp/acw/ui/main/StoreFragment.java
+++ acwandroid/app/src/main/java/com/zapp/acw/ui/main/StoreFragment.java
@@ -1,5 +1,7 @@
 package com.zapp.acw.ui.main;
 
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.zapp.acw.R;
 
@@ -31,8 +34,30 @@ public class StoreFragment extends Fragment {
 	@Override
 	public void onActivityCreated (@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated (savedInstanceState);
-		mViewModel = ViewModelProviders.of (this).get (StoreViewModel.class);
-		// TODO: Use the ViewModel
-	}
 
+		mViewModel = ViewModelProviders.of (this).get (StoreViewModel.class);
+
+		final FragmentActivity activity = getActivity ();
+
+		mViewModel.getAction ().observe (getViewLifecycleOwner (), new Observer<Integer> () {
+			@Override
+			public void onChanged (Integer action) {
+				switch (action) {
+					case StoreViewModel.LOAD_STORE_ENDED: {
+						WebView webView = activity.findViewById (R.id.store_webview);
+						webView.loadDataWithBaseURL ("file:///android_asset/",
+							mViewModel.getConsiderationsHTML (),
+							"text/html",
+							"UTF-8",
+							"about:blank");
+						break;
+					}
+					default:
+						break;
+				}
+			}
+		});
+
+		mViewModel.startLoad (activity);
+	}
 }
--- acwandroid/app/src/main/java/com/zapp/acw/ui/main/StoreViewModel.java
+++ acwandroid/app/src/main/java/com/zapp/acw/ui/main/StoreViewModel.java
@@ -1,7 +1,97 @@
 package com.zapp.acw.ui.main;
 
+import android.app.Activity;
+import android.content.res.Resources;
+import android.util.Log;
+
+import com.zapp.acw.R;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.RawRes;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class StoreViewModel extends ViewModel {
-	// TODO: Implement the ViewModel
+	public static final int LOAD_STORE_ENDED = 1;
+
+	private MutableLiveData<Integer> _action = new MutableLiveData<> ();
+
+	private String _considerationsHTML;
+	private String _privacyPolicyHTML;
+	private String _termsOfUseHTML;
+
+	public MutableLiveData<Integer> getAction () {
+		return _action;
+	}
+
+	public String getConsiderationsHTML () {
+		return _considerationsHTML;
+	}
+
+	public String getPrivacyPolicyHTML () {
+		return _privacyPolicyHTML;
+	}
+
+	public String getTermsOfUseHTML () {
+		return _termsOfUseHTML;
+	}
+
+	public void startLoad (final Activity activity) {
+		new Thread (new Runnable () {
+			@Override
+			public void run () {
+				_considerationsHTML = readRAWResourceFile (activity, R.raw.considerations);
+				_privacyPolicyHTML = readRAWResourceFile (activity, R.raw.privacy_policy);
+				_termsOfUseHTML = readRAWResourceFile (activity, R.raw.terms_of_use);
+
+				_action.postValue (LOAD_STORE_ENDED);
+			}
+		}).start ();
+	}
+
+	private static String readRAWResourceFile (Activity activity, @RawRes int id) {
+		InputStream is = null;
+		try {
+			is = activity.getResources ().openRawResource (id);
+
+			ArrayList<Byte> content = new ArrayList<> ();
+			int avail = 0;
+			while ((avail = is.available ()) > 0) {
+				byte[] buffer = new byte[avail];
+				if (is.read (buffer) != avail) {
+					return null;
+				}
+
+				for (byte b : buffer) {
+					content.add (b);
+				}
+			}
+
+			byte[] fullContent = new byte[content.size ()];
+			for (int i = 0, iEnd = content.size (); i < iEnd; ++i) {
+				fullContent[i] = content.get (i);
+			}
+
+			return new String (fullContent);
+		} catch (Exception ex) {
+			Log.e ("FileUtils", "readRAWResourceFile exception (1): " + ex);
+		} finally {
+			try {
+				if (is != null) {
+					is.close ();
+				}
+			} catch (Exception ex) {
+				Log.e ("FileUtils", "readRAWResourceFile exception (2): " + ex);
+			}
+		}
+
+		return null;
+	}
 }
